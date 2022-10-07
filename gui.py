9b4e188b08ac1a64c52cc2840a0b79c22d551173
Tortole <xorid@mail.ru>
2022-10-07 08:53:28 +0800

    fix move tree element

--- gui.py
+++ gui.py
@@ -22,7 +22,6 @@ def elements_elevator(tree, direction):
     if direction == 'up':
         iter = -1
     elif direction == 'down':
-        # len(parent_node.children)-1
         iter = 1
         select_elems = select_elems[::-1]
 
@@ -32,7 +31,9 @@ def elements_elevator(tree, direction):
     # Условие перемещение:
     # если элементы не у вехнего края при перемещении вверх
     # и не у нижнего края при перемещении вниз
-    if tree_root.children.index(treedata.tree_dict[tree.IdToKey[select_elems[0]]]) not in [0, len(tree_root.children)-1]:
+    index_first_element = tree_root.children.index(treedata.tree_dict[tree.IdToKey[select_elems[0]]])
+    if not ((direction == 'up' and index_first_element == 0) or
+            (direction == 'down' and index_first_element == len(tree_root.children)-1)):
         # Перемещение всех выбранных элементов
         for s_e in select_elems:
             # Получение отмеченного элемента
@@ -73,7 +74,6 @@ actions_tracker = ActionsTrack()
 
 layout = [
     [
-        # sg.Button('Up'), sg.Button('Down'), 
         sg.Button('Move Up'), sg.Button('Move Down'), 
         sg.Button('Add'), sg.Button('Delete'), 
         sg.Button('Change'), sg.Button('Clear'),
@@ -85,7 +85,6 @@ layout = [
         # select_mode=sg.TABLE_SELECT_MODE_BROWSE,
     )],
     [sg.Button('Run'), sg.Button('Load'), sg.Button('Save')],
-    # [sg.Button('Run'), sg.Button('Record')],
 ]
 
 window = sg.Window('Holop', layout, resizable=True, size=(1000, 1000), finalize=True)
@@ -119,64 +118,3 @@ while True:
         print('Save not work yet')
 
 window.close()
-
-
-# import base64
-# from io import BytesIO
-# from PIL import Image
-# import PySimpleGUI as sg
-
-# def update(x0, y0, x1, y1):
-#     """
-#     Update rectangle information
-#     """
-#     print(repr(x0), repr(y0), repr(x1), repr(y1))
-#     window['-START-'].update(f'Start: ({x0}, {y0})')
-#     window['-STOP-' ].update(f'Start: ({x1}, {y1})')
-#     window['-BOX-'  ].update(f'Box: ({abs(x1-x0+1)}, {abs(y1-y0+1)})')
-
-# # Generate an image with size (400, 400)
-# imgdata = base64.b64decode(sg.EMOJI_BASE64_HAPPY_LAUGH)
-# image = Image.open(BytesIO(imgdata))
-# im = image.resize((400, 400), resample=Image.CUBIC)
-# with BytesIO() as output:
-#     im.save(output, format="PNG")
-#     data = output.getvalue()
-
-# layout = [
-#     [sg.Graph((400, 400), (0, 0), (400, 400), key='-GRAPH-',
-#         drag_submits=True, enable_events=True, background_color='green')],
-#     [sg.Text("Start: None", key="-START-"),
-#      sg.Text("Stop: None",  key="-STOP-"),
-#      sg.Text("Box: None",   key="-BOX-")],
-# ]
-
-# window = sg.Window("Measure", layout, finalize=True)
-# graph = window['-GRAPH-']
-# graph.draw_image(data=data, location=(0, 400))
-# x0, y0 = None, None
-# x1, y1 = None, None
-# colors = ['blue', 'white']
-# index = False
-# figure = None
-# while True:
-
-#     event, values = window.read(timeout=100)
-
-#     if event == sg.WINDOW_CLOSED:
-#         break
-#     elif event in ('-GRAPH-', '-GRAPH-+UP'):
-#         if (x0, y0) == (None, None):
-#             x0, y0 = values['-GRAPH-']
-#         x1, y1 = values['-GRAPH-']
-#         update(x0, y0, x1, y1)
-#         if event == '-GRAPH-+UP':
-#             x0, y0 = None, None
-
-#     if figure:
-#         graph.delete_figure(figure)
-#     if None not in (x0, y0, x1, y1):
-#         figure = graph.draw_rectangle((x0, y0), (x1, y1), line_color=colors[index])
-#         index = not index
-
-# window.close()
